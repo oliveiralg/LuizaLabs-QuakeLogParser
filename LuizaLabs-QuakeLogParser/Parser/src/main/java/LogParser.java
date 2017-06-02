d15a280816387043f672f464f1981d56c86b20e7
Leonardo Oliveira <leo_oliveira@yahoo.com>
2017-06-02 10:42:25 -0300

    -Removendo redundância de classes -Implementação da classe de teste

--- LuizaLabs-QuakeLogParser/Parser/src/main/java/LogParser.java
+++ LuizaLabs-QuakeLogParser/Parser/src/main/java/LogParser.java
@@ -18,116 +18,38 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
-import bean.Kill;
-import bean.Partida;
-import bean.Player;
+import com.luizalabs.spring.model.Kill;
+import com.luizalabs.spring.model.Partida;
+import com.luizalabs.spring.model.Player;
 
 @SpringBootApplication
 public class LogParser {
 
   private static final Logger log = LoggerFactory.getLogger(LogParser.class);
 
-  public static void main(String args[]) {
-    if (args.length != 1) {
-      log.info(
-          "Para usar o parser, passe o caminho do arquivo por parametro. Ex: java LogParser \"C:\\quake.log\"");
-    } else {
-      try {
-        log.debug("Abrindo o arquivo!");
-        FileInputStream fstream = new FileInputStream(args[0]);
-        DataInputStream in = new DataInputStream(fstream);
-        BufferedReader br = new BufferedReader(new InputStreamReader(in));
-        String strLine;
-        List<Partida> listaPartidas = new ArrayList<Partida>();
-        List<Player> listaPlayers = new ArrayList<Player>();
-        List<Kill> listaKills = new ArrayList<Kill>();
-        Map<String, Integer> kills = new HashMap<String, Integer>();
-        int totalKills = 0;
-        Partida partida = null;
+  public static List<Partida> ProcessaArquivo(String arquivo) {
+    List<Partida> listaPartidas = new ArrayList<Partida>();
+    try {
+      log.debug("Abrindo o arquivo!");
+      FileInputStream fstream = new FileInputStream(arquivo);
+      DataInputStream in = new DataInputStream(fstream);
+      BufferedReader br = new BufferedReader(new InputStreamReader(in));
+      String strLine;
+      List<Player> listaPlayers = new ArrayList<Player>();
+      List<Kill> listaKills = new ArrayList<Kill>();
+      Map<String, Integer> kills = new HashMap<String, Integer>();
+      int totalKills = 0;
+      Partida partida = null;
 
-        log.debug("Arquivo aberto. Pegando suas linhas para fazer o parse!");
-        while ((strLine = br.readLine()) != null) {
-          log.debug("Encontrado inicio de jogo!"); // Inicio de um jogo
-          if (strLine.indexOf("InitGame") > -1) {
-            // Não finalizou a partida com ShutdownGame
-            if (partida != null) {
-              log.debug("Partida anterior não foi finalizado graciosamente!");
-              listaPlayers = new ArrayList<Player>();
-              listaKills = new ArrayList<Kill>();
-              log.debug("Loop na lista de kills para adicionar players e kills ao objeto partida");
-              for (Map.Entry<String, Integer> entry : kills.entrySet()) {
-                if (!entry.getKey().equals("<world>")) {
-                  log.debug("Adicionando player");// Adiciona o jogador
-                  Player player = new Player();
-                  player.setNome(entry.getKey());
-                  listaPlayers.add(player);
-
-                  log.debug("Adicionando suas kills");// Adiciona suas kills
-                  Kill kill = new Kill();
-                  kill.setPlayer(player);
-                  kill.setQtde(entry.getValue());
-                  listaKills.add(kill);
-                }
-              }
-
-              partida.setPlayers(listaPlayers.toArray(new Player[0]));
-              partida.setKills(listaKills.toArray(new Kill[0]));
-              log.debug("Setando o total de kills");
-              partida.setTotalKills(totalKills);
-              log.debug("Adicionando a partida na lista");
-              listaPartidas.add(partida);
-            }
-            log.debug("Resetando variáveis para nova partida!");
-            partida = new Partida();
-            totalKills = 0;
-            kills = new HashMap<String, Integer>();
-          }
-          // Player
-          else if (strLine.indexOf("ClientUserinfoChanged") > -1) {
-            log.debug("Novo player entrou na partida!");// Pega o nome do player
-            Matcher m = Pattern.compile("n\\\\(.*?)\\\\t").matcher(strLine);
-            if (m.find()) {
-              String player = m.group(1).trim();
-              // Adiciona o player com 0 kill
-              if (!kills.containsKey(player)) {
-                kills.put(player, 0);
-              }
-            }
-          }
-          // Kill
-          else if (strLine.indexOf("Kill") > -1) {
-            log.debug("Nova morte aconteceu!");
-            totalKills++;
-
-            // Usa regexp para pegar quem matou e foi morto
-            Matcher m =
-                Pattern.compile(":.*?:.*?:(.*?)\\S*killed\\S*(.*?)\\S*by?").matcher(strLine);
-            if (m.find()) {
-              String matou = m.group(1).trim();
-              String morto = m.group(2).trim();
-              if (matou.equals("<world>") || matou.equals(morto)) {
-                log.debug("Suicídio ou morto pelo mapa! Perde frag.");
-                if (kills.containsKey(morto)) {
-                  kills.replace(morto, kills.get(morto) - 1);
-                } else {
-                  kills.put(morto, -1);
-                }
-              } else {
-                log.debug("Computando morte para player!");
-                if (kills.containsKey(matou)) {
-                  kills.replace(matou, kills.get(matou) + 1);
-                } else {
-                  kills.put(matou, 1);
-                }
-              }
-            }
-          }
-          // Fim da partida
-          else if (strLine.indexOf("ShutdownGame") > -1) {
-            log.debug("Partida anterior encerrada!");
+      log.debug("Arquivo aberto. Pegando suas linhas para fazer o parse!");
+      while ((strLine = br.readLine()) != null) {
+        log.debug("Encontrado inicio de jogo!"); // Inicio de um jogo
+        if (strLine.indexOf("InitGame") > -1) {
+          // Não finalizou a partida com ShutdownGame
+          if (partida != null) {
+            log.debug("Partida anterior não foi finalizado graciosamente!");
             listaPlayers = new ArrayList<Player>();
             listaKills = new ArrayList<Kill>();
-
             log.debug("Loop na lista de kills para adicionar players e kills ao objeto partida");
             for (Map.Entry<String, Integer> entry : kills.entrySet()) {
               if (!entry.getKey().equals("<world>")) {
@@ -150,23 +72,113 @@ public class LogParser {
             partida.setTotalKills(totalKills);
             log.debug("Adicionando a partida na lista");
             listaPartidas.add(partida);
-            log.debug("Resetando variáveis para nova partida!");
-            partida = null;
-            totalKills = 0;
-            kills = new HashMap<String, Integer>();
+          }
+          log.debug("Resetando variáveis para nova partida!");
+          partida = new Partida();
+          totalKills = 0;
+          kills = new HashMap<String, Integer>();
+        }
+        // Player
+        else if (strLine.indexOf("ClientUserinfoChanged") > -1) {
+          log.debug("Novo player entrou na partida!");// Pega o nome do player
+          Matcher m = Pattern.compile("n\\\\(.*?)\\\\t").matcher(strLine);
+          if (m.find()) {
+            String player = m.group(1).trim();
+            // Adiciona o player com 0 kill
+            if (!kills.containsKey(player)) {
+              kills.put(player, 0);
+            }
           }
         }
-        br.close();
+        // Kill
+        else if (strLine.indexOf("Kill") > -1) {
+          log.debug("Nova morte aconteceu!");
+          totalKills++;
 
-        log.debug("Passando lista para WebService REST persistir em banco!");
-        RestTemplate rest = new RestTemplate();
-        String url = "http://localhost:8080/partidas";
-        @SuppressWarnings("rawtypes")
-        ResponseEntity<List> response = rest.postForEntity(url, listaPartidas, List.class);
+          // Usa regexp para pegar quem matou e foi morto
+          Matcher m = Pattern.compile(":.*?:.*?:(.*?)\\S*killed\\S*(.*?)\\S*by?").matcher(strLine);
+          if (m.find()) {
+            String matou = m.group(1).trim();
+            String morto = m.group(2).trim();
+            if (matou.equals("<world>") || matou.equals(morto)) {
+              log.debug("Suicídio ou morto pelo mapa! Perde frag.");
+              if (kills.containsKey(morto)) {
+                kills.replace(morto, kills.get(morto) - 1);
+              } else {
+                kills.put(morto, -1);
+              }
+            } else {
+              log.debug("Computando morte para player!");
+              if (kills.containsKey(matou)) {
+                kills.replace(matou, kills.get(matou) + 1);
+              } else {
+                kills.put(matou, 1);
+              }
+            }
+          }
+        }
+        // Fim da partida
+        else if (strLine.indexOf("ShutdownGame") > -1) {
+          log.debug("Partida anterior encerrada!");
+          listaPlayers = new ArrayList<Player>();
+          listaKills = new ArrayList<Kill>();
+
+          log.debug("Loop na lista de kills para adicionar players e kills ao objeto partida");
+          for (Map.Entry<String, Integer> entry : kills.entrySet()) {
+            if (!entry.getKey().equals("<world>")) {
+              log.debug("Adicionando player");// Adiciona o jogador
+              Player player = new Player();
+              player.setNome(entry.getKey());
+              listaPlayers.add(player);
+
+              log.debug("Adicionando suas kills");// Adiciona suas kills
+              Kill kill = new Kill();
+              kill.setPlayer(player);
+              kill.setQtde(entry.getValue());
+              listaKills.add(kill);
+            }
+          }
+
+          partida.setPlayers(listaPlayers.toArray(new Player[0]));
+          partida.setKills(listaKills.toArray(new Kill[0]));
+          log.debug("Setando o total de kills");
+          partida.setTotalKills(totalKills);
+          log.debug("Adicionando a partida na lista");
+          listaPartidas.add(partida);
+          log.debug("Resetando variáveis para nova partida!");
+          partida = null;
+          totalKills = 0;
+          kills = new HashMap<String, Integer>();
+        }
+      }
+      br.close();
+    } catch (IOException e) {
+      log.error("Arquivo " + arquivo + " não encontrado!");
+      return null;
+    }
+    return listaPartidas;
+  }
+
+  @SuppressWarnings("rawtypes")
+  public static List PersisteLista(List<Partida> lista) {
+    log.debug("Passando lista para WebService REST persistir em banco!");
+    RestTemplate rest = new RestTemplate();
+    String url = "http://localhost:8080/partidas";
+    ResponseEntity<List> response = rest.postForEntity(url, lista, List.class);
+    return response.getBody();
+  }
+
+  @SuppressWarnings("rawtypes")
+  public static void main(String args[]) {
+    if (args.length != 1) {
+      log.info(
+          "Para usar o parser, passe o caminho do arquivo por parametro. Ex: java LogParser \"C:\\quake.log\"");
+    } else {
+      List<Partida> listaPartidas = ProcessaArquivo(args[0]);
+      if (listaPartidas != null) {
         log.debug("Mostrando retorno do WebService.");
-        System.out.println(response.getBody());
-      } catch (IOException e) {
-        log.error("Arquivo " + args[0] + " não encontrado!");
+        List retorno = PersisteLista(listaPartidas);
+        System.out.println(retorno);
       }
     }
   }
--- LuizaLabs-QuakeLogParser/Testes/src/main/java/Testes.java
+++ LuizaLabs-QuakeLogParser/Testes/src/main/java/Testes.java
@@ -1,16 +1,6 @@
 package main.java;
 
-import java.io.BufferedReader;
-import java.io.DataInputStream;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,157 +8,116 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
-import bean.Kill;
-import bean.Partida;
-import bean.Player;
+import com.luizalabs.spring.model.Partida;
 
 @SpringBootApplication
 public class Testes {
 
   private static final Logger log = LoggerFactory.getLogger(Testes.class);
 
-  private static void CarregarArquivo() {
-    try {
-      FileInputStream fstream = new FileInputStream("games1.log");
-      DataInputStream in = new DataInputStream(fstream);
-      BufferedReader br = new BufferedReader(new InputStreamReader(in));
-      String strLine;
-      List<Partida> listaPartidas = new ArrayList<Partida>();
-      List<Player> listaPlayers = new ArrayList<Player>();
-      List<Kill> listaKills = new ArrayList<Kill>();
-      Map<String, Integer> kills = new HashMap<String, Integer>();
-      int totalKills = 0;
-      Partida partida = null;
-
-      while ((strLine = br.readLine()) != null) {
-        // Inicio de um jogo
-        if (strLine.indexOf("InitGame") > -1) {
-          // Não finalizou a partida com ShutdownGame
-          if (partida != null) {
-            listaPlayers = new ArrayList<Player>();
-            listaKills = new ArrayList<Kill>();
-
-            for (Map.Entry<String, Integer> entry : kills.entrySet()) {
-              if (!entry.getKey().equals("<world>")) {
-                // Adiciona o jogador
-                Player player = new Player();
-                player.setNome(entry.getKey());
-                listaPlayers.add(player);
-
-                // Adiciona suas kills
-                Kill kill = new Kill();
-                kill.setPlayer(player);
-                kill.setQtde(entry.getValue());
-                listaKills.add(kill);
-              }
-            }
-
-            partida.setPlayers(listaPlayers.toArray(new Player[0]));
-            partida.setKills(listaKills.toArray(new Kill[0]));
-            partida.setTotalKills(totalKills);
-            listaPartidas.add(partida);
-          }
-          partida = new Partida();
-          totalKills = 0;
-          kills = new HashMap<String, Integer>();
-        }
-        // Player
-        else if (strLine.indexOf("ClientUserinfoChanged") > -1) {
-          // Pega o nome do player
-          Matcher m = Pattern.compile("n\\\\(.*?)\\\\t").matcher(strLine);
-          if (m.find()) {
-            String player = m.group(1).trim();
-            // Adiciona o player com 0 kill
-            if (!kills.containsKey(player)) {
-              kills.put(player, 0);
-            }
-          }
-        }
-        // Kill
-        else if (strLine.indexOf("Kill") > -1) {
-          totalKills++;
-
-          // Usa regexp para pegar quem matou e foi morto
-          Matcher m = Pattern.compile(":.*?:.*?:(.*?)\\S*killed\\S*(.*?)\\S*by?").matcher(strLine);
-          if (m.find()) {
-            String matou = m.group(1).trim();
-            String morto = m.group(2).trim();
-            if (matou.equals("<world>") || matou.equals(morto)) {
-              if (kills.containsKey(morto)) {
-                kills.replace(morto, kills.get(morto) - 1);
-              } else {
-                kills.put(morto, -1);
-              }
-            } else {
-              if (kills.containsKey(matou)) {
-                kills.replace(matou, kills.get(matou) + 1);
-              } else {
-                kills.put(matou, 1);
-              }
-            }
-          }
-        }
-        // Fim da partida
-        else if (strLine.indexOf("ShutdownGame") > -1) {
-          listaPlayers = new ArrayList<Player>();
-          listaKills = new ArrayList<Kill>();
-
-          for (Map.Entry<String, Integer> entry : kills.entrySet()) {
-            if (!entry.getKey().equals("<world>")) {
-              // Adiciona o jogador
-              Player player = new Player();
-              player.setNome(entry.getKey());
-              listaPlayers.add(player);
-
-              // Adiciona suas kills
-              Kill kill = new Kill();
-              kill.setPlayer(player);
-              kill.setQtde(entry.getValue());
-              listaKills.add(kill);
-            }
-          }
-
-          partida.setPlayers(listaPlayers.toArray(new Player[0]));
-          partida.setKills(listaKills.toArray(new Kill[0]));
-          partida.setTotalKills(totalKills);
-          listaPartidas.add(partida);
-          partida = null;
-          totalKills = 0;
-          kills = new HashMap<String, Integer>();
-        }
-      }
-      br.close();
-
-      RestTemplate rest = new RestTemplate();
-      String url = "http://localhost:8080/partidas";
-      rest.postForEntity(url, listaPartidas, List.class);
-    } catch (IOException e) {
-      log.info("Arquivo não encontrado!");
-    }
-  }
-
+  @SuppressWarnings({"rawtypes"})
   public static void main(String args[]) {
-    //Remove todos os registros do banco
+    // Remove todos os registros do banco
+    log.debug("Limpando o banco!");
     RestTemplate rest = new RestTemplate();
     String url = "http://localhost:8080/cleandatabase";
     rest.getForEntity(url, null);
-    
+
     // Carrega arquivo para iniciar o banco;
-    CarregarArquivo();
-    // Verifica se o arquivo foi carregado com sucesso buscando todas as partidas
-    url = "http://localhost:8080/partidas";
-    @SuppressWarnings("rawtypes")
-    ResponseEntity<List> response = rest.getForEntity(url, List.class);
-       
-    System.out.println(response.getBody());
-    // Pega o id e faz a busca pelo id agora
+    log.debug("Processando o arquivo");
+    List<Partida> listaPartidas = LogParser.ProcessaArquivo(
+        "C:/Development/Repositories/LuizaLabs-QuakeLogParser/Testes/src/main/java/games1.log");
+    if (listaPartidas != null) {
+      log.debug("Gravando no banco a lista");
+      LogParser.PersisteLista(listaPartidas);
+      // Verifica se o arquivo foi carregado com sucesso buscando todas as partidas
+      log.debug("Buscando todas as partidas");
+      url = "http://localhost:8080/listarpartidas";
+      ResponseEntity<Partida[]> responseLista = rest.getForEntity(url, Partida[].class);
+      Partida partida = null;
+      if (responseLista.getStatusCode().value() == 200) // Chamada ocorreu OK
+      {
+        // Check if the totalKills = 105, Size kills = 4 e size Players = 4
+        partida = responseLista.getBody()[0];
+        if (partida != null && partida.getTotalKills() == 105 && partida.getKills().length == 4
+            && partida.getPlayers().length == 4)
+          log.info("Teste OK. Partida retornada é a carregada!");
+        else {
+          log.error("Partida carregada não confere. Teste falhou!");
+          return;
+        }
+      } else {
+        log.error("Chamada para pegar partidas falhou!");
+        return;
+      }
+      // Pega o id e faz a busca pelo id agora
+      int id = partida.getId();
+      log.debug("Buscando a partida pelo id = " + id);
+      url = "http://localhost:8080/partida/" + id;
+      ResponseEntity<Partida> responsePartida = rest.getForEntity(url, Partida.class);
+      if (responsePartida.getStatusCode().value() == 200) // Chamada ocorreu OK
+      {
+        // Check if the totalKills = 105, Size kills = 4 e size Players = 4
+        partida = responsePartida.getBody();
+        if (partida != null && partida.getTotalKills() == 105 && partida.getKills().length == 4
+            && partida.getPlayers().length == 4)
+          log.info("Teste OK. Partida retornada é a correta!");
+        else {
+          log.error("Partida retornada não confere. Teste falhou!");
+          return;
+        }
+      } else {
+        log.error("Chamada para pegar partida id = " + id + " falhou!");
+        return;
+      }
+      // Faz a busca por player
+      log.debug("Buscando partida por player = Isgalamido");
+      url = "http://localhost:8080/player/Isgalamido";
+      responseLista = rest.getForEntity(url, Partida[].class);
+      if (responseLista.getStatusCode().value() == 200) // Chamada ocorreu OK
+      {
+        // Check if the totalKills = 105, Size kills = 4 e size Players = 4
+        partida = responseLista.getBody()[0];
+        if (partida != null && partida.getTotalKills() == 105 && partida.getKills().length == 4
+            && partida.getPlayers().length == 4)
+          log.info("Teste OK. Partida retornada é a correta!");
+        else {
+          log.error("Partida retornada não confere. Teste falhou!");
+          return;
+        }
+      } else {
+        log.error("Chamada para pegar partida por player = Isgalamido falhou!");
+        return;
+      }
 
-    // Faz a busca por player
+      // Remove o registro
+      log.debug("Removendo a partida do banco");
+      url = "http://localhost:8080/removerpartida/" + id;
+      ResponseEntity response = rest.getForEntity(url, null);
+      if (response.getStatusCode().value() != 200) // Ocorreu problema na chamada
+      {
+        log.error("Chamada para remover falhou!");
+        return;
+      } else
+        log.info("Teste Ok. Chamada para remover partida executada");
 
-    // Remove o registro
+      // Verifica se banco foi limpo buscando todas as partidas
+      log.debug("Buscando todas as partidas");
+      url = "http://localhost:8080/listarpartidas";
+      responseLista = rest.getForEntity(url, Partida[].class);
+      if (responseLista.getStatusCode().value() != 200) // Ocorreu erro
+      {
+        log.error("Ocorreu erro ao buscar as partidas!");
+        return;
 
-    // Verifica se banco foi limpo buscando todas as partidas
+      } else if (responseLista.getBody().length != 0) // Ocorreu erro
+      {
+        log.error("A quantidade de partidas retornadas não corresponde. Teste falhou!");
+        return;
+      } else
+        log.info("Teste Ok. Nenhuma partida retornada");
 
-
+      log.info("Todos os testes passaram!");
+    }
   }
 }
